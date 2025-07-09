71b1d37c36bcfce4d89d84c126fa91b7506ae147
Misha <misha@test>
2025-07-09 17:56:49 +0200

    feat: re-designed tests as LiveGames is a UI component

--- src/components/LiveGames/index.test.tsx
+++ src/components/LiveGames/index.test.tsx
@@ -1,6 +1,8 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import { LiveGames } from "./index";
+import { type Game } from "@/types/game";
+
 describe("LiveGames", () => {
   it("shows placeholder when gamesList is empty", () => {
     render(<LiveGames gamesList={[]} />);
@@ -10,19 +12,30 @@ describe("LiveGames", () => {
     ).toBeInTheDocument();
   });
 
-  it("adds new game and displays GameCard", () => {
-    render(<LiveGames gamesList={[]} />);
+  it("renders a list of games", () => {
+    const mockGames: Game[] = [
+      {
+        homeName: "Spain",
+        awayName: "Italy",
+        homeScore: 2,
+        awayScore: 1,
+      },
+      {
+        homeName: "Germany",
+        awayName: "France",
+        homeScore: 0,
+        awayScore: 0,
+      },
+    ];
 
-    const homeInput = screen.getByLabelText(/home team/i);
-    const awayInput = screen.getByLabelText(/away team/i);
-    const button = screen.getByRole("button", { name: /add game/i });
-
-    fireEvent.change(homeInput, { target: { value: "Spain" } });
-    fireEvent.change(awayInput, { target: { value: "Italy" } });
-    fireEvent.click(button);
+    render(<LiveGames gamesList={mockGames} />);
 
     expect(screen.getByText("Spain")).toBeInTheDocument();
     expect(screen.getByText("Italy")).toBeInTheDocument();
+    expect(screen.getByText("2 : 1")).toBeInTheDocument();
+
+    expect(screen.getByText("Germany")).toBeInTheDocument();
+    expect(screen.getByText("France")).toBeInTheDocument();
     expect(screen.getByText("0 : 0")).toBeInTheDocument();
   });
 });
